e2dcc91ba3c4ec5bedbf733cdbef7989469af1f1
cmaldonadoa <cristobal.maldonado.a@gmail.com>
2020-08-03 04:36:37 -0400

    Fixed file uploading

--- src/ModelUploader.js
+++ src/ModelUploader.js
@@ -3,14 +3,24 @@ import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import LinearProgress from "@material-ui/core/LinearProgress";
+import TextField from "@material-ui/core/TextField";
 
 export default function ModelUploader() {
   const [loading, setLoading] = React.useState(false);
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
   const [file, setFile] = React.useState(null);
-  const [name, setName] = React.useState("");
+
+  const onChangeUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
   const sendFile = (event) => {
-    if (file) {
+    if (file && username && password) {
       setLoading(true);
       fetch("https://bimapi.velociti.cl/authenticate", {
         method: "POST",
@@ -18,19 +28,18 @@ export default function ModelUploader() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          user: "bim_test",
-          password: "test",
+          user: username,
+          password: password,
         }),
       })
         .then((res) => res.json())
         .then((res) => {
           let formData = new FormData();
-          formData.append("bim", file, name);
+          formData.append("bim", file);
           return fetch("https://bimapi.velociti.cl/dev_new_model", {
             method: "POST",
             headers: {
-              Authorization: res.token,
-              'Content-Type': 'multipart/form-data',
+              Authorization: res.token
             },
             body: formData,
           });
@@ -45,7 +54,6 @@ export default function ModelUploader() {
 
   const selectFile = (event) => {
     setFile(event.target.files[0]);
-    setName(event.target.value);
   };
 
   return (
@@ -67,9 +75,15 @@ export default function ModelUploader() {
             textAlign: "center",
           }}
         >
-          <Grid container spacing={3} justify="center">
+          <Grid container spacing={5} justify="center">
             <Grid item xs={12}>
-              <input type="file" onChange={selectFile} />
+              <TextField label="Usuario" onChange={onChangeUsername} />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField label="Contraseña" type="password" onChange={onChangePassword} />
+            </Grid>
+            <Grid item xs={12}>
+              <input type="file" onChange={selectFile} accept=".ifc, .ifczip" />
             </Grid>
             <Grid item xs={12}>
               <Button variant="contained" color="primary" onClick={sendFile}>
