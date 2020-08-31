91fe1f956664c86ef1edc9ba7d3d0d6689d2e143
Cristobal <cristobal.maldonado.a@gmail.com>
2020-08-31 16:21:31 -0400

    Make names editable and rows deleteable

--- src/components/ProjectBar.js
+++ src/components/ProjectBar.js
@@ -1,168 +1,39 @@
 import React from "react";
-import { withStyles, makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import OpenInNewIcon from "@material-ui/icons/OpenInNew";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
+import EditIcon from "@material-ui/icons/Edit";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import MuiAccordion from "@material-ui/core/Accordion";
 import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
 import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import Typography from "@material-ui/core/Typography";
-import PropTypes from "prop-types";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
-import Paper from "@material-ui/core/Paper";
 import CheckCircleOutlineOutlinedIcon from "@material-ui/icons/CheckCircleOutlineOutlined";
+import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
+import MaterialTable from "material-table";
+import TextField from "@material-ui/core/TextField";
+import CheckIcon from "@material-ui/icons/Check";
+import CloseIcon from "@material-ui/icons/Close";
 
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
+function useInterval(callback, delay) {
+  const savedCallback = React.useRef();
 
-function getComparator(order, orderBy) {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
+  // Remember the latest callback.
+  React.useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-function EnhancedTableHead(props) {
-  const { classes, order, orderBy, onRequestSort, headCells } = props;
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "default"}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-};
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-  },
-  paper: {
-    width: "100%",
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: "rect(0 0 0 0)",
-    height: 1,
-    margin: -1,
-    overflow: "hidden",
-    padding: 0,
-    position: "absolute",
-    top: 20,
-    width: 1,
-  },
-}));
-
-function EnhancedTable(props) {
-  const classes = useStyles();
-  const [order, setOrder] = React.useState("asc");
-  const [orderBy, setOrderBy] = React.useState("calories");
-  const { headCells, rows } = props;
-
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <TableContainer>
-          <Table
-            className={classes.table}
-            aria-labelledby="tableTitle"
-            size={"small"}
-            aria-label="enhanced table"
-          >
-            <EnhancedTableHead
-              classes={classes}
-              order={order}
-              orderBy={orderBy}
-              onRequestSort={handleRequestSort}
-              headCells={headCells}
-            />
-            <TableBody>
-              {stableSort(rows, getComparator(order, orderBy)).map(
-                (row, index) => {
-                  return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                      {Object.keys(row).map((attr, idx) =>
-                        attr === "id" ? null : (
-                          <TableCell key={row.id + "-" + idx} align="left">
-                            {row[attr]}
-                          </TableCell>
-                        )
-                      )}
-                    </TableRow>
-                  );
-                }
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </div>
-  );
+  // Set up the interval.
+  React.useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
 
 const Accordion = withStyles({
@@ -200,6 +71,9 @@ const AccordionSummary = withStyles({
 const AccordionDetails = withStyles((theme) => ({
   root: {
     padding: theme.spacing(2),
+    "& div": {
+      width: "100%",
+    },
   },
 }))(MuiAccordionDetails);
 
@@ -208,35 +82,56 @@ const ProjectBar = (props) => {
   const [loading, setLoading] = React.useState(true);
   const [models, setModels] = React.useState([]);
   const [expanded, setExpanded] = React.useState(false);
-  const [pending, setPending] = React.useState(0);
-  const name = props.name;
+  const [request, setRequest] = React.useState(false);
+  const [editing, setEditing] = React.useState(false);
+  const [name, setName] = React.useState(props.name);
+  const [newName, setNewName] = React.useState(props.name);
   const id = props.id;
   const onAdd = props.onAdd;
   const onOpen = props.onOpen;
 
+  const formatData = (data) => {
+    return data.info.map((model) => {
+      return {
+        id: model.id,
+        tag: model.tag,
+        name: model.name,
+        filename: model.original_name,
+        loading: model.loading,
+        status: model.error ? (
+          <div
+            className="d-flex align-items-center"
+            style={{ color: "#f50057" }}
+          >
+            <ErrorOutlineIcon fontSize="small" />{" "}
+            <span className="ml-2">Error</span>
+          </div>
+        ) : model.loading ? (
+          <div className="d-flex align-items-center">
+            <CircularProgress style={{ margin: 2 }} color="action" size={16} />
+            <span className="ml-2">Cargando</span>
+          </div>
+        ) : (
+          <div
+            className="d-flex align-items-center"
+            style={{ color: "#20e43b" }}
+          >
+            <CheckCircleOutlineOutlinedIcon fontSize="small" />{" "}
+            <span className="ml-2">Listo</span>
+          </div>
+        ),
+      };
+    });
+  };
+
   React.useEffect(() => {
-    if (expanded && pending > 0) {
-      setTimeout(() => {
-        fetch(`https://bimapi.velociti.cl/dev_get_projects/${id}/info`, {
-          headers: {
-            Authorization: "public_auth",
-          },
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            let info = data.info;
-            setModels(info);
-            setPending(info.filter((row) => row.loading).length);
-          })
-          .catch((err) => console.error(err));
-      }, 5000);
+    if (!loading) {
+      setRequest(true);
     }
   });
 
-  const handleExpand = () => {
-    setExpanded(!expanded);
-    if (!expanded) {
-      setLoading(true);
+  useInterval(() => {
+    if (request) {
       fetch(`https://bimapi.velociti.cl/dev_get_projects/${id}/info`, {
         headers: {
           Authorization: "public_auth",
@@ -244,12 +139,38 @@ const ProjectBar = (props) => {
       })
         .then((res) => res.json())
         .then((data) => {
-          setLoading(false);
-          let info = data.info;
-          setModels(info);
-          setPending(info.filter((row) => row.loading).length);
+          const oldLoading = models.filter((model) => model.loading);
+          const newLoading = data.info.filter((model) => model.loading);
+          if (
+            models.length !== data.info.length ||
+            oldLoading.length !== newLoading.length
+          ) {
+            const info = formatData(data);
+            setModels(info);
+          }
         })
         .catch((err) => console.error(err));
+    }
+  }, 5000);
+
+  const handleExpand = (newState) => {
+    setExpanded(newState);
+    if (newState) {
+      fetch(`https://bimapi.velociti.cl/dev_get_projects/${id}/info`, {
+        headers: {
+          Authorization: "public_auth",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const info = formatData(data);
+          setLoading(false);
+          setModels(info);
+        })
+        .catch((err) => console.error(err));
+    } else {
+      setLoading(true);
+      setRequest(false);
     }
   };
 
@@ -257,91 +178,173 @@ const ProjectBar = (props) => {
     return null;
   }
   return (
-    <Accordion onChange={handleExpand}>
+    <Accordion expanded={expanded} onChange={() => handleExpand(!expanded)}>
       <AccordionSummary expandIcon={<ExpandMoreIcon />}>
         <div className="d-flex justify-content-between align-items-center w-100">
-          <div>
-            <h5>{name}</h5>
+          <div className="d-flex align-items-center">
+            {editing ? (
+              <React.Fragment>
+                <TextField
+                  label="Nombre del proyecto"
+                  value={newName}
+                  onClick={(event) => event.stopPropagation()}
+                  onChange={(event) => setNewName(event.target.value)}
+                />
+                <IconButton
+                  className="ml-2"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setName(newName);
+                    setEditing(false);
+                  }}
+                >
+                  <CheckIcon color="action" fontSize="small" />
+                </IconButton>
+                <IconButton
+                  className="ml-2"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setEditing(false);
+                  }}
+                >
+                  <CloseIcon color="action" fontSize="small" />
+                </IconButton>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <h5>{name}</h5>
+                <IconButton
+                  className="ml-2"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setEditing(true);
+                  }}
+                >
+                  <EditIcon color="action" fontSize="small" />
+                </IconButton>
+              </React.Fragment>
+            )}
           </div>
           <div
             className="d-flex justify-content-around"
             style={{ width: "120px" }}
           >
-            <IconButton onClick={onAdd}>
+            <IconButton
+              onClick={(event) => {
+                event.stopPropagation();
+                onAdd(() => handleExpand(true));                
+              }}
+            >
               <AddCircleIcon />
             </IconButton>
-            <IconButton onClick={onOpen}>
+            <IconButton
+              onClick={(event) => {
+                event.stopPropagation();
+                onOpen();
+              }}
+            >
               <OpenInNewIcon />
             </IconButton>
           </div>
         </div>
       </AccordionSummary>
       <AccordionDetails>
-        {loading ? (
-          <div className="w-100 d-flex justify-content-center">
-            <CircularProgress color="inherit" size={16} />
-          </div>
-        ) : models.length === 0 ? (
-          <Typography>No se han cargado modelos a este proyecto.</Typography>
-        ) : (
-          <EnhancedTable
-            rows={models.map((model) => {
-              return {
-                id: model.id,
-                tag: model.tag,
-                name: model.name,
-                filename: model.original_name,
-                loading: model.loading ? (
-                  <div
-                    className="d-flex align-items-center"
-                    style={{ color: "#f50057" }}
-                  >
-                    <CircularProgress
-                      style={{ margin: 2 }}
-                      color="secondary"
-                      size={16}
-                    />
-                    <span className="ml-2">Cargando</span>
-                  </div>
-                ) : (
-                  <div
-                    className="d-flex align-items-center"
-                    style={{ color: "#20e43b" }}
-                  >
-                    <CheckCircleOutlineOutlinedIcon fontSize="small" />{" "}
-                    <span className="ml-2">Listo</span>
-                  </div>
-                ),
-              };
-            })}
-            headCells={[
-              {
-                id: "tag",
-                numeric: false,
-                disablePadding: false,
-                label: "Tipo",
+        <MaterialTable
+          title=""
+          isLoading={loading}
+          columns={[
+            {
+              field: "tag",
+              title: "Tipo",
+              lookup: { ARC: "ARC", STR: "STR", MEP: "MEP" },
+            },
+            {
+              field: "name",
+              title: "Nombre",
+            },
+            {
+              field: "filename",
+              title: "Archivo",
+              editable: "never",
+            },
+            {
+              field: "status",
+              title: "Estado",
+              editable: "never",
+              sorting: false,
+            },
+          ]}
+          data={models}
+          editable={{
+            onRowDelete: (oldData) => {
+              const id = oldData.id;
+              return fetch(`https://bimapi.velociti.cl/dev_model/${id}`, {
+                method: "DELETE",
+                headers: {
+                  Authorization: "public_auth",
+                },
+              })
+                .then((res) => res.json())
+                .then((data) => {
+                  const index = oldData.tableData.id;
+                  let newModels = models;
+                  delete newModels[index];
+                  newModels = newModels.filter((model) => Boolean(model));
+                  setModels(newModels);
+                })
+                .catch((err) => console.error(err));
+            },
+          }}
+          cellEditable={{
+            cellStyle: {},
+            onCellEditApproved: (newValue, oldValue, rowData, columnDef) => {
+              const field = columnDef.field;
+              let formData = new FormData();
+              formData.append(field, newValue);
+              formData.append("id", rowData.id);
+
+              return fetch(`https://bimapi.velociti.cl/dev_patch_model`, {
+                method: "POST",
+                headers: {
+                  Authorization: "public_auth",
+                },
+                body: formData,
+              })
+                .then((res) => res.json())
+                .then((data) => {
+                  const index = rowData.tableData.id;
+                  let newModels = models;
+                  let model = newModels[index];
+                  model[field] = newValue;
+                  newModels[index] = model;
+                  setModels(newModels);
+                })
+                .catch((err) => console.error(err));
+            },
+          }}
+          options={{
+            toolbar: false,
+            showTitle: false,
+            paging: false,
+            actionsColumnIndex: 4,
+          }}
+          localization={{
+            header: {
+              actions: "Acciones",
+            },
+            body: {
+              deleteTooltip: "Eliminar",
+              editTooltip: "Editar",
+              emptyDataSourceMessage:
+                "No se han cargado modelos a este proyecto.",
+              editRow: {
+                deleteText: "¿Desea eliminar este modelo?",
+                cancelTooltip: "Cancelar",
+                saveTooltip: "Continuar",
               },
-              {
-                id: "name",
-                numeric: false,
-                disablePadding: false,
-                label: "Nombre",
-              },
-              {
-                id: "filename",
-                numeric: false,
-                disablePadding: false,
-                label: "Archivo",
-              },
-              {
-                id: "status",
-                numeric: false,
-                disablePadding: false,
-                label: "Estado",
-              },
-            ]}
-          />
-        )}
+            },
+          }}
+        />
       </AccordionDetails>
     </Accordion>
   );
